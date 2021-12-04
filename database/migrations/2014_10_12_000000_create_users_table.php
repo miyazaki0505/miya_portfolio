a2558111a8228e4bdda47f236edf8b4a5c767e96
miyazaki0505 <mr.gingerman0505@gmail.com>
2021-12-04 11:11:02 +0000

    UsersTableMigration修正

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -29,7 +29,7 @@ class CreateUsersTable extends Migration
             $table->integer('language');
             $table->text('about_myself');
             $table->rememberToken();
-            $table->timestamp('deleted_at');
+            $table->softDeletes();
             $table->timestamps();
         });
     }
