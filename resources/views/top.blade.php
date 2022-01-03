7bf13368144067057d8941b66b68dff98c8ad8b8
miyazaki0505 <mr.gingerman0505@gmail.com>
2022-01-03 03:03:27 +0000

    詳細ボタンの位置修正

--- resources/views/top.blade.php
+++ resources/views/top.blade.php
@@ -12,41 +12,43 @@
     @foreach($projects as $project)
     
     <div class="card m-5">
+        <div class="m-4 row">
+            <div class="col-10">
 
-        <div class="m-4">
-
-            <h4>
-                @foreach($languages as $key => $language)
-                    @if($key == $project->language)
-                        【{{ $language }}】
-                        @php
-                            break;
-                        @endphp
-                    @endif
-                @endforeach
-                {{ $project->project_name }}
-            </h4>
-            <div class="row m-1 pt-4">
-                <div class="col-xs-12">
-                    @foreach($work_locations as $key => $work_location)
-                        @if($key == $project->work_location)
-                        <div class="ml-5">勤務地：{{ $work_location }}</div>
+                <h4>
+                    @foreach($languages as $key => $language)
+                        @if($key == $project->language)
+                            【{{ $language }}】
                             @php
                                 break;
                             @endphp
                         @endif
                     @endforeach
+                    {{ $project->project_name }}
+                </h4>
+                <div class="row m-1 pt-4">
+                    <div class="col-xs-12">
+                        @foreach($work_locations as $key => $work_location)
+                            @if($key == $project->work_location)
+                            <div class="ml-4">勤務地：{{ $work_location }}</div>
+                                @php
+                                    break;
+                                @endphp
+                            @endif
+                        @endforeach
+                    </div>
+
+                    <div class="col-xs-12 ml-4">月額報酬：￥{{ number_format($project->unit_price) }}〜</div>
                 </div>
-
-                <div class="col-xs-12 ml-5">月額報酬：￥{{ number_format($project->unit_price) }}〜</div>
             </div>
 
-            <div class="text-right">
-                <a href="{{ config('app.url'). "/showDetail/". $project->id }}" type="button" class="btn btn-outline-secondary ml-4">詳細</a>
+            
+            <div class="text-right d-flex align-items-center">
+                <div class="col-sm-12"><a href="{{ config('app.url'). "/showDetail/". $project->id }}" type="button" class="btn btn-outline-secondary ml-4">詳細</a></div>
             </div>
+ 
             
         </div>
-
     </div>
 
     @endforeach
