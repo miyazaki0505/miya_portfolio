afd50597860a8a57e4ec6d0a3eee0e4eb16c143b
miyazaki0505 <mr.gingerman0505@gmail.com>
2022-01-05 14:14:47 +0000

    マッチする案件がない際の表示に関しての質問

--- app/Http/Controllers/ProjectsController.php
+++ app/Http/Controllers/ProjectsController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Project;
+use App\User;
 use App\Company;
 use App\Skill;
 use App\OtherRequirement;
@@ -11,6 +12,20 @@ use App\OtherRequirement;
 
 class ProjectsController extends Controller
 {
+    public function index()
+    {
+        $projects = Project::orderBy('id', 'asc')->paginate(5);
+
+        $languages = config('language');
+        $work_locations = config('work_location');
+
+        return view('users.top', [
+            "projects" => $projects,
+            "languages" => $languages,
+            "work_locations" => $work_locations,
+        ]);
+    }
+
     public function show($id)
     {
         $project = Project::findOrFail($id);
@@ -24,14 +39,45 @@ class ProjectsController extends Controller
 
         $data = [
             "project" => $project,
-                "languages" => $languages,
-                "occupations" => $occupations,
-                "work_locations" => $work_locations,
-                "skills" => $skills,
-                "other_requirements" => $other_requirements,
-                "company" => $company,
+            "languages" => $languages,
+            "occupations" => $occupations,
+            "work_locations" => $work_locations,
+            "skills" => $skills,
+            "other_requirements" => $other_requirements,
+            "company" => $company,
         ];
     
         return view('projects.project_detail', $data);
+    }
+
+    public function match()
+    {
+        $users = \Auth::user();
+        $user_last_name = $users->last_name;
+        $user_work_location = $users->work_location;
+        $user_occupation = $users->occupation;
+        $user_language = $users->language;
+
+        $query = Project::query();
+        $query->where('work_location', $user_work_location); 
+        $query->where('occupation', $user_occupation);
+        $query->where('language', $user_language);
+        $projects = $query->get();
+
+        $languages = config('language');
+        $work_locations = config('work_location');
+
+        $data = [
+            "users" => $users,
+            "projects" => $projects,
+            "user_last_name" => $user_last_name,
+            "user_work_location" => $user_work_location,
+            "user_occupation" => $user_occupation,
+            "user_language" => $user_language,
+            "languages" => $languages,
+            "work_locations" => $work_locations,
+        ];
+    
+        return view('users.match', $data);
     }
 }
--- resources/views/users/match.blade.php
+++ resources/views/users/match.blade.php
@@ -2,71 +2,60 @@
 
 @section('content')
 
-    <div class="text-right">
-        <div class="btn-group">
-            <div class="btn-toolbar pr-5" role="toolbar">
-                <div class="matching mr-3"><a href="{{ config('app.url'). '/match' }}" type="button" class="btn btn-outline-danger">マッチング</a></div>
-                <div class="like mr-3"><a href="#" type="button" class="btn btn-outline-danger">いいね</a></div>
-                <div class="history mr-3"><a href="#" type="button" class="btn btn-outline-danger">応募履歴</a></div>
-            </div>
-        </div>
-    </div>
-
-    <div class="text-center mt-4">
-        <h2>マッチング</h2>
-        <p>{{ Auth::user()->last_name."さんのご希望の条件と一致する案件はこちらです。" }}</p>
-    </div>
+    @include('users.tabs')
 
     @foreach($projects as $project)
 
-        @foreach($users as $user)
+    <div class="text-center mt-4">
+        <h2>マッチング</h2>
+        <p>{{ $user_last_name. "さんのご希望の条件と一致する案件はこちらです。" }}</p>
+    </div>
 
-            @if( $user->language == $project->language && $user->work_location == $project->work_location && $user->occupation == $project->occupation)
+    <div class="card m-5">
+        <div class="m-4 row">
+            <div class="col-md-10">
 
-            <div class="card m-5">
-                <div class="m-4 row">
-                    <div class="col-10">
-
-                        <h4>
-                            @foreach($languages as $key => $language)
-                                @if($key == $project->language)
-                                    【{{ $language }}】
-                                    @php
-                                        break;
-                                    @endphp
-                                @endif
-                            @endforeach
-                            {{ $project->project_name }}
-                        </h4>
-                        <div class="row m-1 pt-4">
-                            <div class="col-xs-12">
-                                @foreach($work_locations as $key => $work_location)
-                                    @if($key == $project->work_location)
-                                    <div class="ml-4">勤務地：{{ $work_location }}</div>
-                                        @php
-                                            break;
-                                        @endphp
-                                    @endif
-                                @endforeach
-                            </div>
-
-                            <div class="col-xs-12 ml-4">月額報酬：￥{{ number_format($project->unit_price) }}〜</div>
-                        </div>
+                <h4>
+                    @foreach($languages as $key => $language)
+                        @if($key === $project->language)
+                            【{{ $language }}】
+                            @php
+                                break;
+                            @endphp
+                        @endif
+                    @endforeach
+                    {{ $project->project_name }}
+                </h4>
+                <div class="m-1 pt-4 row">
+                    <div class="col-sx-12">
+                        @foreach($work_locations as $key => $work_location)
+                            @if($key === $project->work_location)
+                            <div class="ml-4">勤務地：{{ $work_location }}</div>
+                                @php
+                                    break;
+                                @endphp
+                            @endif
+                        @endforeach
                     </div>
 
-                    <div class="text-right d-flex align-items-center">
-                        <div class="col-sm-12"><a href="{{ config('app.url'). "/showDetail/". $project->id }}" type="button" class="btn btn-outline-secondary ml-4">詳細</a></div>
-                    </div>
-        
-                </div>
+                    <div class="col-sx-12 ml-4">月額報酬：￥{{ number_format($project->unit_price) }}〜</div>
+                </div>     
+            </div>
+            
+            <div class="text-right d-flex align-items-center col-md-2">
+                <a href="{{ config('app.url'). "/showDetail/". $project->id }}" type="button" class="btn btn-outline-secondary ml-4">詳細</a>
             </div>
 
-            @endif
-
-        @endforeach
+        </div>
+    </div>
 
     @endforeach
 
-    <div class="pagination justify-content-center">{{ $projects->links() }}</div>
+    @if(empty($projects))
+        <div class="text-center mt-4">
+            <h2>マッチング</h2>
+            <p class="mt-4">{{ "現在、". $user_last_name. "さんのご希望の条件と一致する案件はございませんでした。" }}</p>
+        </div>
+    @endif
 
 @endsection
